d87331821ca306885cba1d2857a21d5c3205352b
Jay <jvanord@indasysllc.com>
2021-02-02 21:56:11 -0500

    some layout work

---
+++ vue-app/src/components/Error.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-snackbar
+    v-model="error"
+    :timeout="-1"
+    :value="true"
+    absolute
+    top
+    right
+    color="error"
+    outlined
+  >
+    {{ error }}
+    <template v-slot:action="{ attrs }">
+      <v-btn color="error" text v-bind="attrs" @click="clearError">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { mapActions, mapGetters } from "vuex";
+export default Vue.extend({
+  data: () => ({}),
+  methods: {
+    ...mapActions(["clearError"]),
+  },
+  computed: {
+    ...mapGetters(["error"]),
+  },
+});
+</script>
--- vue-app/src/store/index.ts
+++ vue-app/src/store/index.ts
@@ -1,14 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
 
 Vue.use(Vuex)
 
+const initialState = () => ({ user: null, error: null })
+
 export default new Vuex.Store({
-  state: {
+  state: initialState(),
+  getters: {
+    isAuthenticated: (state) => !!state.user,
+    error: (state) => state.error
   },
   mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    },
+    setError(state, payload) {
+      state.error = payload;
+    }
   },
   actions: {
+    clearError({commit}, payload) {
+      commit("setError", "");
+    },
+    signUpAction({ commit }, payload) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(payload.email, payload.password)
+        .then(response => {
+          commit("setUser", response.user);
+        })
+        .catch(error => {
+          commit("setError", error.message);
+        });
+    }
   },
   modules: {
   }

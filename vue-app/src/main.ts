8105a1db18708b1502852525b530e71b9d4ea9de
Jay <jvanord@indasysllc.com>
2021-02-02 19:40:56 -0500

    added firebase config/init

--- vue-app/src/main.ts
+++ vue-app/src/main.ts
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
+import config from './../../config.json'
 
 Vue.config.productionTip = false
 
@@ -10,5 +12,8 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp(config.firebase)
+  }
 }).$mount('#app')

42104dff811b025d31d72eb36796132ed5f5546b
Jay <jvanord@indasysllc.com>
2021-02-07 07:46:33 -0500

    moved "home" to "about"

--- vue-app/src/router/index.ts
+++ vue-app/src/router/index.ts
@@ -33,7 +33,7 @@ const routes: Array<RouteConfig> = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-        meta: { authRequired: true }
+        meta: { authRequired: false }
     },
     { path: '/404', name: 'NotFound', component: NotFound },
     { path: '*', component: NotFound }

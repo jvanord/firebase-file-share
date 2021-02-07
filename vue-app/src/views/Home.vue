f7eb2875ba7e711e6805c0b0732a4a091372c219
Jay <jvanord@indasysllc.com>
2021-02-07 09:50:22 -0500

    file list on home page

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
 	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+		<v-container class="pa-5">
+			<h1 class="text-center mb-5">Your Files</h1>
+
+			<div v-if="loading" class="pa-10 text-center">
+				<v-progress-circular color="primary" indeterminate></v-progress-circular>
+			</div>
+
+			<v-card v-if="empty && !loading" class="pa-4 text-center">
+				<v-card-text>
+					<p>
+						You have no files yet to view.
+						<router-link to="/upload">Upload</router-link> one now.
+					</p>
+				</v-card-text>
+			</v-card>
+
+			<v-lazy v-for="file in myFiles" :key="file.id">
+				<v-card class="pa-2 ma-1">
+					{{ file.name }}
+				</v-card>
+			</v-lazy>
+		</v-container>
 	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './../components/HelloWorld.vue'; // @ is an alias to /src
-
-@Component({
-	components: {
-		HelloWorld,
+import Vue from 'vue';
+import { mapGetters } from 'vuex';
+import VuetifyLogo from '../../../web/components/VuetifyLogo.vue';
+export default Vue.extend({
+	name: 'Home',
+	components: { VuetifyLogo },
+	mounted() {
+		this.$store.dispatch('file/load');
+		this.hideLoading(2000);
 	},
-})
-export default class Home extends Vue {}
+	data: () => ({
+		loading: true,
+	}),
+	computed: {
+		...mapGetters({ myFiles: 'file/myFiles' }),
+		empty(): boolean {
+			return !this.myFiles.length;
+		},
+	},
+	methods: {
+		hideLoading: function (timeout: number | undefined) {
+			let _this = this;
+			if (timeout)
+				setTimeout(function () {
+					_this.loading = false;
+				}, timeout);
+			else _this.loading = false;
+		},
+	},
+});
 </script>
